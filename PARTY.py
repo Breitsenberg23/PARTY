ef53d6a9a9a2ea614339f2ff96c29954ca805116
Breitsenberg23 <102793487+Breitsenberg23@users.noreply.github.com>
2022-04-01 11:31:14 +0200

    Add files via upload

---
+++ PARTY.py
@@ -0,0 +1,12 @@
+wetter = input ("Wochenendwetter - 'sonnig' oder 'regnerisch': ")
+
+if wetter.upper() == "SONNIG":
+    print("Gartenparty")
+    print("yeeeeah")
+    
+elif wetter.lower() == "regnerisch":
+    print("scheibe")
+    print("Kellerparty")
+    
+else:
+    print("bitte 'regnerisch' oder 'sonnig' eingeben")
